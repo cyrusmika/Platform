a63ed8f2a63757bcb700aa2291c9e605d97a715e
cyrusmika <60200502+cyrusmika@users.noreply.github.com>
2020-02-12 18:01:22 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,36 +1,31 @@
-
 let hero = new Hero()
-let shapes = []
 
+let keyPressed = {}
 ERASE()
 hero.draw()
-
 window.addEventListener("keydown", event => {
-	if (event.code === 'KeyS'){
-		let colorIndex = Math.random()
-		let color = "red"
-		if (colorIndex < 0.5) {
-			color = "orange"
-		}
-		if (colorIndex < 0.4){
-			color = "yellow"
-		}
-		CTX.fillStyle = color
-
-		let x = Math.random() * CANVAS.width
-		let y = Math.random() * CANVAS.height
-		let height = Math.random() * 500
-		let width = Math.random() * 500
-		let s = new Shape(x, y, height, width)
-		shapes.push(s)
-	}
-	if (event.code === "ArrowRight") {
-		hero.moveRight()
-	}
-
-	ERASE()
-	shapes.forEach(s => s.draw())
-	hero.draw()
-
+	keyPressed[event.code] = true 
 })
+window.addEventListener("keyup", event => {
+	keyPressed[event.code] = false
+})
+function loop(){
+	if (keyPressed["ArrowRight"]) {
+		hero.moveRight()	
+	}
+	if (keyPressed["ArrowLeft"]) {
+		hero.moveLeft()
+	}
+	if (keyPressed["ArrowUp"]) {
+		hero.moveUp()
+	}
+	if (keyPressed["ArrowDown"]) {
+		hero.moveDown()
+	}
+	ERASE()
+
+	hero.draw()
+	setTimeout(loop, 1000/60)
+}
+loop()
 
